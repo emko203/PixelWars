07e15012d0e8a223da811ebc4a91128bbc7262ec
Karapachov,Emanuil E.C <e.karapachov@student.fontys.nl>
2020-04-09 14:41:41 +0200

    ScriptableObjectsFolderStructure
    Added couple of new Folders

---
+++ PixelWars/Assets/Scripts/Cards/SpellDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellDisplay : MonoBehaviour
+{
+    public Spell spell;
+
+    public Text nameText;
+    public Text descriptionText;
+
+    public Image artworkImage;
+
+    public Text manaText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = spell.name;
+        descriptionText.text = spell.description;
+
+        artworkImage.sprite = spell.artwork;
+
+        manaText.text = spell.manaCost.ToString();
+
+    }
+
+
+}
