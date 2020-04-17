2bfec6700ca846fe3cbd163cdc309f3d87047c7f
Luc Somers <lucsomers@hotmail.com>
2020-04-17 17:32:49 +0200

    Added player

---
+++ PixelWars/Assets/Scripts/SecondTryGamePlay/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int currentMana = 0;
+    private int currentHealth = 1;
+    private int maxMana = 0;
+    private int maxHealth = 0;
+
+    public Player(int maxMana, int maxHealth)
+    {
+        this.maxMana = maxMana;
+        this.maxHealth = maxHealth;
+
+        this.currentHealth = maxHealth;
+        this.currentMana = maxMana;
+    }
+
+    public void PayMana(int ManaCost)
+    {
+        currentMana -= ManaCost;
+    }
+
+    public void takeDamage(int amount)
+    {
+        currentHealth -= amount;
+    }
+
+    public bool IsDead()
+    {
+        if (currentHealth <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int Mana { get => currentMana;}
+    public int Health { get => currentHealth;}
+}
