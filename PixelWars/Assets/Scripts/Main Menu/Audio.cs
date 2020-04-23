b577c5d030e2504cbc7426f3f4a2df45c4f12923
Karapachov,Emanuil E.C <e.karapachov@student.fontys.nl>
2020-04-23 12:14:36 +0200

    AddedBackgroundMusic

---
+++ PixelWars/Assets/Scripts/Main Menu/Audio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    private static Audio instance = null;
+
+    public static Audio Instance 
+    {
+        get{ return instance; }
+    }
+
+    void Awake() 
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else 
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
